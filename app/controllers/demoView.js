505d2d7fa7783cd16aabc751d4ac3a6a06ae237a
Rufino Rojas <lucras015@hotmail.com>
2018-10-18 18:50:08 -0500

    nuevos cambios

--- app/controllers/demoView.js
+++ app/controllers/demoView.js
@@ -26,33 +26,5 @@ var args = $.args;
 	// $.indicatorNavBar.setIndicatorColor(colors[_.random(0, colors.length - 1)]);
 // }
 
-import { logger } from 'logger';
 
-/**
- * The scoped constructor of the controller.
- **/
-(function constructor() {
 
-}());
-
-let focussedTextfield;
-
-function textFieldValueChanged({ source, value }) {
-	logger.log(`${source.id} changed value to ${value}`);
-}
-
-function textFieldFocussed({ source }) {
-	focussedTextfield = source.id;
-	logger.log(`${source.id} focussed!`);
-}
-
-function textFieldBlurred({ source }) {
-	focussedTextfield = null;
-	logger.log(`${source.id} blurred!`);
-}
-
-function blurTextfield() {
-	if (focussedTextfield) {
-		$[focussedTextfield].blur();
-	}
-}
